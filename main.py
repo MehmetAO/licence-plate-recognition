b696b5f98f0a381c15c2483ef14cf2555cda4a23
Mehmet Ali <109021192+MehmetAO@users.noreply.github.com>
2022-07-23 19:23:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import LPR
+import serial
+import time
+
+# For windows PC use COM* ports / For linux use /dev/tty*
+
+#arduinoData = serial.Serial("/dev/ttyUSB0", 9600)
+arduinoData = serial.Serial("COM4", 9600)
+
+time.sleep(3)
+
+plate = LPR.main("test.jpg")
+
+on = "1"
+off = "0"
+
+if plate == ["3","5","A","D","4","5","9","7"]:
+    arduinoData.write(on.encode())
