ec7aa9c00452666de3756ca9abdd2206ecf7bf89
Mehmet Ali <109021192+MehmetAO@users.noreply.github.com>
2022-07-24 12:29:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import LPR
+import liveLPR
 import serial
 import time
 
 # For windows PC use COM* ports / For linux use /dev/tty*
 
-#arduinoData = serial.Serial("/dev/ttyUSB0", 9600)
-arduinoData = serial.Serial("COM4", 9600)
+arduinoData = serial.Serial("/dev/ttyUSB0", 9600)
+#arduinoData = serial.Serial("COM4", 9600)
 
 time.sleep(3)
 
-plate = LPR.main("test.jpg")
+plate = liveLPR.main()
 
 on = "1"
 off = "0"
